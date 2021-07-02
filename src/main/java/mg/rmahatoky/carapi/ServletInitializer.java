ae98ad2947e08b69d72de1118e47f18359e28a20
Mahatoky <mahatoky.rasolonirina@pulse.mg>
2021-07-02 23:51:46 +0300

    initialization

---
+++ src/main/java/mg/rmahatoky/carapi/ServletInitializer.java
@@ -0,0 +1,13 @@
+package mg.rmahatoky.carapi;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CarApiApplication.class);
+	}
+
+}
