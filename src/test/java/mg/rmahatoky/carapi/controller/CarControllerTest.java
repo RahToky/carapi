3adb18d2a82d9b6a627499c495b9ded68276a368
Mahatoky Rasolonirina <mahatoky.rasolonirina@gmail.com>
2021-08-24 16:19:36 +0300

    - unused import

--- src/test/java/mg/rmahatoky/carapi/controller/CarControllerTest.java
+++ src/test/java/mg/rmahatoky/carapi/controller/CarControllerTest.java
@@ -1,23 +1,16 @@
 package mg.rmahatoky.carapi.controller;
 
-import mg.rmahatoky.carapi.CarApiApplication;
 import mg.rmahatoky.carapi.config.WebMvcConfig;
 import mg.rmahatoky.carapi.service.ICarService;
-import mg.rmahatoky.carapi.service.impl.CarService;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
